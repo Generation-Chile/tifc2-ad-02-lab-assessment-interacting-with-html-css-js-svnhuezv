77aacc56d979398982643fe93c118a0e852d5c17
Raúl Ernesto Sanhueza Manríquez <resm5233@gmail.com>
2025-04-23 17:55:40 -0400

    Trabajo terminado

---
+++ script.js
@@ -0,0 +1,15 @@
+
+document.getElementById("encabezado").textContent = ("Adiós") //Le cambiamos el nombre al h1 que lleva como id=encabezado.
+
+document.getElementById("saludo").style.color = "orange" //Le cambiamos el color a la etiqueta h3 que lleva como id=saludo.
+
+const charge = document.getElementById("charge")
+
+charge.addEventListener("click",()=> {charge.style.color = "brown"}) //Añadimos la función a la etiqueta h6 para que cambie de color haciéndole un click encima.
+
+//Sintaxis del método = "nameID.addEventListener("click",()=> {"nameID".style.color = "color a elegir"})
+
+
+
+
+
